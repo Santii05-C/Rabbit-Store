cefd1319cfcbbc8522d7a367bff7c4a8e9086e5b
Santii05-C <santi.c.pgm@gmail.com>
2025-06-03 22:18:31 -0300

    last commit

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+
 const MyOrdersPage = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const mockOrders = [
+        {
+          _id: "12345",
+          createdAt: new Date(),
+          shippingAddress: { city: "New York", country: "USA" },
+          ordersItems: [
+            {
+              name: "Product 1",
+              image: "https://picsum.photos/500/500?random=1",
+            },
+          ],
+          totalPrice: 100,
+          isPaid: true,
+        },
+      ];
+    });
+  });
+  //3:36
   return <div>MyOrdersPage</div>;
 };
 
