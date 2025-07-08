4df5c7d11594b997ac99640635ea7fdb3805a1f1
Santii05-C <santi.c.pgm@gmail.com>
2025-07-07 23:39:31 -0300

    last commit

--- frontend/src/components/Products/FilterSideBar.jsx
+++ frontend/src/components/Products/FilterSideBar.jsx
@@ -1,11 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const FilterSideBar = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [filters, setFilters] = useState();
+  const [filters, setFilters] = useState({
+    category: "",
+    gender: "",
+    color: "",
+    size: [],
+    material: [],
+    brand: [],
+    minPrice: 0,
+    maxPrice: 100,
+  });
+
+  const [priceRange, setPriceRange] = useState([0, 100]);
+
+  const categories = ["Top Wear", "Bottom Wear"];
+
+  const colors = [
+    "Red",
+    "Blue",
+    "Black",
+    "Green",
+    "Yellow",
+    "Gray",
+    "White",
+    "Pink",
+    "Beige",
+    "Navy",
+  ];
+
+  const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+
+  const materials = [
+    "Cotton",
+    "Wool",
+    "Denim",
+    "Polyester",
+    "Silk",
+    "Limen",
+    "Viscose",
+    "Fleece",
+  ];
+
+  const brands = [
+    "Urban Threads",
+    "Modern Fit",
+    "Street Style",
+    "Beach Breeze",
+    "Fashionista",
+    "ChicStyle",
+  ];
+
+  const gender = ["Men", "Women"];
+
+  useEffect(() => {
+    const params = Object.fromEntries([...searchParams]);
+
+    setFilters({
+      category: params.category || "",
+      gender: params.gender || "",
+      color: params.color || "",
+      size: params.size ? params.size.split(",") : [],
+      material: params.material ? params.material.split(",") : [],
+      brand: params.brand ? params.brand.split(",") : [],
+      minPrice: params.minPrice || 0,
+      maxPrice: params.maxPrice || 100,
+    });
+    setPriceRange([0, params.maxPrice || 100]);
+  }, [searchParams]);
 
   return <div>FilterSideBar</div>;
 };
 
 export default FilterSideBar;
+//4:6
