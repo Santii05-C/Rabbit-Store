4eb0eea1476eaf31321b63d5fcec5cc006096ada
Santii05-C <santi.c.pgm@gmail.com>
2025-09-13 16:33:40 -0300

    last commit

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const cart = {
@@ -22,6 +23,7 @@ const cart = {
 
 const Checkout = () => {
   const navigate = useNavigate();
+  const [checkoutId, setCheckoutId] = useState(null);
   const [shippingAddress, setShippingAddress] = useState({
     firstName: "",
     lastName: "",
@@ -33,7 +35,144 @@ const Checkout = () => {
   });
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter">
-      //4:44 Checkout
+      {/* Left Section */}
+      <div className="bg-white rounded-lg p-6">
+        <h2 className="text-2xl uppercase mb-6">Checkout</h2>
+        <form>
+          <h3 className="text-lg mb-4">Content Details</h3>
+          <div className="mb-4">
+            <label className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              value="user@example.com"
+              className="w-full p-2 border rounded"
+              disabled
+            />
+          </div>
+          <h3 className="text-lg mb-4">Delivery</h3>
+          <div className="mb-4 grid grid-cols-2 gap-4">
+            <div>
+              <label className="block text-gray-700">First Name</label>
+              <input
+                type="text"
+                value={setShippingAddress.firstName}
+                onChange={(e) =>
+                  setShippingAddress({
+                    ...shippingAddress,
+                    firstName: e.target.value,
+                  })
+                }
+                className="w-full p-2 border rounded"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700">Last Name</label>
+              <input
+                type="text"
+                value={setShippingAddress.lastName}
+                onChange={(e) =>
+                  setShippingAddress({
+                    ...shippingAddress,
+                    lastName: e.target.value,
+                  })
+                }
+                className="w-full p-2 border rounded"
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label className="black text-gray-700">Address</label>
+            <input
+              type="text"
+              value={shippingAddress.address}
+              onChange={(e) =>
+                setShippingAddress({
+                  ...shippingAddress,
+                  address: e.target.value,
+                })
+              }
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4 grid grid-cols-2 gap-4">
+            <div>
+              <label className="block text-gray-700">City</label>
+              <input
+                type="text"
+                value={setShippingAddress.city}
+                onChange={(e) =>
+                  setShippingAddress({
+                    ...shippingAddress,
+                    city: e.target.value,
+                  })
+                }
+                className="w-full p-2 border rounded"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700">Postal Code</label>
+              <input
+                type="text"
+                value={setShippingAddress.postalCode}
+                onChange={(e) =>
+                  setShippingAddress({
+                    ...shippingAddress,
+                    postalCode: e.target.value,
+                  })
+                }
+                className="w-full p-2 border rounded"
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label className="black text-gray-700">Country</label>
+            <input
+              type="text"
+              value={shippingAddress.country}
+              onChange={(e) =>
+                setShippingAddress({
+                  ...shippingAddress,
+                  country: e.target.value,
+                })
+              }
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="black text-gray-700">Phone</label>
+            <input
+              type="number"
+              value={shippingAddress.phone}
+              onChange={(e) =>
+                setShippingAddress({
+                  ...shippingAddress,
+                  phone: e.target.value,
+                })
+              }
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mt-6">
+            {!checkoutId ? (
+              <button
+                type="submit"
+                className="w-full bg-black text-white py-3 rounded"
+              >
+                Continue to Payment
+              </button>
+            ) : (
+              <h3 className=""></h3>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
